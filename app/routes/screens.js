56a13faf5a81ba9c7de0c2b0d830b8631cd6a8d6
Amari1007 <amarighambi@gmail.com>
2024-01-03 15:16:48 +0200

    basic web layout

--- app/routes/screens.js
+++ app/routes/screens.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const route = express.Router();
 
+route.get("/", (req, res) => {
+    res.render("index");
+});
+
 route.get("/home", (req, res) => {
     res.render("index");
 });
